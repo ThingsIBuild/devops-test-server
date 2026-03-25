8dedf57eca8a9945056416b3746207f8e4a84ec3
Gurnav224 <chaudharyg856@gmail.com>
2026-03-25 13:59:14 +0000

    Add port information to home endpoint response

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ def home():
     return {
         "message": "FastAPI running 🚀",
         "container": socket.gethostname(),
+        "port": os.getenv("PORT", "unknown"),
         "server_name": os.getenv("SERVER_NAME", "unknown")
     }
