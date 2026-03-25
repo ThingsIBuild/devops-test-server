3ce1502dd7c7daa6d2ab5eaa603869852aa72723
Gurnav224 <chaudharyg856@gmail.com>
2026-03-25 13:35:34 +0000

    Enhance home endpoint response to include container and server name details

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import socket
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "FastAPI running on EC2 🚀"}
+    return {
+        "message": "FastAPI running 🚀",
+        "container": socket.gethostname(),
+        "server_name": os.getenv("SERVER_NAME", "unknown")
+    }
